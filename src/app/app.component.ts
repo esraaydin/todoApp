c4727ed2206218aff17ff4dd23105b76ba2064c9
esraaydin <esra.aydin.008@outlook.com>
2022-04-12 14:24:54 +0300

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Model, TodoItem} from './model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todoApp';
+  model = new Model();
+  isDisplay=false;
+
+  getName() {
+    return this.model.user;
+  }
+
+  getItems() {
+    if(this.isDisplay){
+      return this.model.items;
+    }
+    return this.model.items.filter((item: { action: any; })=> !item.action);
+  }
+
+  addItem(value: string){
+    if(value!=""){
+      this.model.items.push(new TodoItem(value,false))
+    }
+  }
 }
